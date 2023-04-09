a4278ec4f5506a98cedb8bdd46a422f0ec10ba48
Newton Ombese <newtonombese1@gmail.com>
2023-04-09 23:30:46 +0300

    :wrench:confs: Adding `@Auth0` domain &  client id

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,7 +5,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-eiiitfcjetriy76c.us.auth0.com',            // Auth0 domain
+  clientId: 'weOF9DNI9T9D0Yzn0jpPNkzOB53RDWcb',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
